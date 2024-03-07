3b2b771a3e2fc68d06a269457bd06821dd873293
bfuchs18 <bari.fuchs@gmail.com>
2024-03-07 11:22:36 -0500

    init commit

---
+++ R/score_pmum.R
@@ -0,0 +1,95 @@
+#' score_pmum: Scored data from the Problematic Media Use Measure
+#'
+#' This function scores the Problematic Media Use Measure
+#'
+#' To use this function, the data must be prepared according to the following criteria:
+#' 1) The data must include all individual questionnaire items
+#' 2) The  columns/variables must match the following naming convention: 'pmum#' where # is the question number (1-27).
+#' 3) Questions must have the numeric value for the choices: 1, Never | 2, Rarely | 3, Sometimes | 4, Often | 5, Always
+#'
+#' Note, as long as variable names match those listed, the dataset can include other variables
+#'
+#' @references
+#' Primary References for the child version of the Behavioral Inhibition Scale/Behavioral Activation Scale and Scoring:
+#' Domoff SE, Harrison K, Gearhardt AN, Gentile DA, Lumeng JC, Miller AL. Development and Validation of the Problematic Media Use Measure: A Parent Report Measure of Screen Media "Addiction" in Children. Psychol Pop Media Cult. 2019 Jan;8(1):2-11. doi: 10.1037/ppm0000163. Epub 2017 Nov 16. PMID: 30873299; PMCID: PMC6411079.
+#'
+#' @param pmum_data a data.frame all items for the Problematic Media Use Measure following the naming conventions described above
+#' @inheritParams score_bes
+#'
+#' @return A dataset with total score for the Problematic Media Use Measure
+#' @examples
+#'
+#' # scoring for the pmum with IDs
+#' pmum_score_data <- score_pmum(pmum_data, id = 'ID')
+#'
+#'
+#' \dontrun{
+#' }
+#'
+#' @export
+
+score_pmum <- function(pmum_data, score_base = TRUE, id) {
+  
+  #### 1. Set up/initial checks #####
+  
+  # check that pmum_data exist and is a data.frame
+  data_arg <- methods::hasArg(pmum_data)
+  
+  if (isTRUE(data_arg) & !is.data.frame(pmum_data)) {
+    stop("pmum_data must be entered as a data.frame")
+  } else if (isFALSE(data_arg)) {
+    stop("pmum_data must set to the data.frame with amount consumed for each food item")
+  }
+  
+  # check if id exists
+  ID_arg <- methods::hasArg(id)
+  
+  if (isTRUE(ID_arg)){
+    if (!(id %in% names(pmum_data))) {
+      stop("variable name entered as id is not in pmum_data")
+    }
+  }
+  
+  #### 2. Set Up Data #####
+  
+  # set up database for results create empty matrix
+  pmum_score_dat <- data.frame(pmum_total = rep(NA, nrow(pmum_data)))
+  
+  
+  if (isTRUE(ID_arg)) {
+    pmum_score_dat <- data.frame(pmum_data[[id]], pmum_score_dat)
+    names(pmum_score_dat)[1] <- id
+  }
+  
+  # remove underscore if in column names
+  names(pmum_data) <- gsub('pmum_', 'pmum', names(pmum_data))
+  
+  # get primary questions
+  q_numbers <- seq(1, 27)
+  pmum_primary_qs <- paste0("pmum", q_numbers)
+  
+  # re-scale data
+  pmum_data_edit <- pmum_data
+  
+  if (isTRUE(score_base)){
+    pmum_data_edit[pmum_primary_qs] <- sapply(pmum_primary_qs, function(x) pmum_data[[x]] + 1, simplify = TRUE)
+  }
+  
+  
+  ## Score 
+  
+  pmum_score_dat[["pmum_total"]] <- rowMeans(pmum_data_edit[c(pmum_primary_qs)])
+  
+  #### 3. Clean Export/Scored Data #####
+  
+  ## merge raw responses with scored data
+  if (isTRUE(ID_arg)){
+    pmum_phenotype <- merge(pmum_data, pmum_score_dat, by = id)
+    
+    return(list(score_dat = as.data.frame(pmum_score_dat),
+                bids_phenotype = as.data.frame(pmum_phenotype)))
+  } else {
+    return(list(score_dat = as.data.frame(pmum_score_dat)))
+  }
+}
+
---
+++ R/score_pptq.R
@@ -0,0 +1,125 @@
+#' score_pptq: Scored data from the Pictorial Personality Traits Questionnaire for Children 
+#'
+#' This function scores the Pictorial Personality Traits Questionnaire for Children and provides subscale scores for: Extraversion, Neuroticism, Openness, Conscientiousness, Agreeableness
+#'
+#' To use this function, the data must be prepared according to the following criteria:
+#' 1) The data must include all individual questionnaire items
+#' 2) The  columns/variables must match the following naming convention: 'pptq#' where # is the question number (1-15)
+#' 3) Questions 1-15 must have the numeric value for the choices: 1 - Definitely Yes (left side), 2 - A little bit (left side), 3 - It depends, 4 - A little bit (right side), 5 - Definitely yes (right side)
+#'
+#' Note, as long as variable names match those listed, the dataset can include other variables
+#'
+#' @references
+#' Primary Reference for the Questionnaire and Scoring:
+#' Maćkiewicz M, Cieciuch J. Pictorial Personality Traits Questionnaire for Children (PPTQ-C)-A New Measure of Children's Personality Traits. Front Psychol. 2016 Apr 14;7:498. doi: 10.3389/fpsyg.2016.00498. PMID: 27252661; PMCID: PMC4879772.
+#' 
+#' @param pptq_data a data.frame all items for the Pictorial Personality Traits Questionnaire for Children  following the naming conventions described above
+#' @inheritParams score_bes
+#'
+#' @return A dataset with subscale scores for the Pictorial Personality Traits Questionnaire for Children 
+#' @examples
+#'
+#' # scoring for the pptq with IDs
+#' pptq_score_data <- score_pptq(pptq_data, id = 'ID')
+#'
+#'
+#' \dontrun{
+#' }
+#'
+#' @export
+
+score_pptq <- function(pptq_data, score_base = TRUE, id) {
+  
+  #### 1. Set up/initial checks #####
+  
+  # check that pptq_data exist and is a data.frame
+  data_arg <- methods::hasArg(pptq_data)
+  
+  if (isTRUE(data_arg) & !is.data.frame(pptq_data)) {
+    stop("pptq_data must be entered as a data.frame")
+  } else if (isFALSE(data_arg)) {
+    stop("pptq_data must set to the data.frame with amount consumed for each food item")
+  }
+  
+  # check if id exists
+  ID_arg <- methods::hasArg(id)
+  
+  if (isTRUE(ID_arg)){
+    if (!(id %in% names(pptq_data))) {
+      stop("variable name entered as id is not in pptq_data")
+    }
+  }
+  
+  #### 2. Set Up Data #####
+  
+  # set up database for results create empty matrix
+  pptq_score_dat <- data.frame(pptq_extraversion = rep(NA, nrow(pptq_data)), 
+                               pptq_neuroticism = rep(NA, nrow(pptq_data)), 
+                               pptq_openness = rep(NA, nrow(pptq_data)),
+                               pptq_conscientiousness = rep(NA, nrow(pptq_data)),
+                               pptq_agreeableness = rep(NA, nrow(pptq_data)))
+  
+  if (isTRUE(ID_arg)) {
+    pptq_score_dat <- data.frame(pptq_data[[id]], pptq_score_dat)
+    names(pptq_score_dat)[1] <- id
+  }
+  
+  # remove underscore if in column names
+  names(pptq_data) <- gsub('pptq_', 'pptq', names(pptq_data))
+  
+  # get primary questions to score
+  q_numbers <- seq(1, 15)
+  pptq_primary_qs <- paste0("pptq", q_numbers)
+  
+  # re-scale data
+  pptq_data_edit <- pptq_data
+  
+  if (isTRUE(score_base)){
+    pptq_data_edit[pptq_primary_qs] <- sapply(pptq_primary_qs, function(x) pptq_data[[x]] + 1, simplify = TRUE)
+  }
+  
+  # calculate reversed scores
+  reverse_qs <- c("pptq2", "pptq4", "pptq6", "pptq8", "pptq10", "pptq12", "pptq14")
+  
+  for (var in 1:length(reverse_qs)) {
+    var_name <- reverse_qs[var]
+    reverse_name <- paste0(var_name, "_rev")
+    
+    pptq_data_edit[[reverse_name]] <- ifelse(is.na(pptq_data_edit[[var_name]]), NA, ifelse(pptq_data_edit[[var_name]] == 1, 5, ifelse(pptq_data_edit[[var_name]] == 2, 4, 3)))
+  }
+  
+  ## Score Subscales
+  
+  # Extraversion
+  ex_vars <- c("pptq1", "pptq6_rev", "pptq11")
+  pptq_score_dat[["pptq_extraversion"]] <- rowSums(pptq_data_edit[ex_vars])
+  
+  # Neuroticism
+  neurot_vars <- c("pptq2_rev", "pptq7", "pptq12_rev")
+  pptq_score_dat[["pptq_neuroticism"]] <- rowSums(pptq_data_edit[neurot_vars])
+  
+  # Openness
+  open_vars <- c("pptq3", "pptq8_rev", "pptq13")
+  pptq_score_dat[["pptq_openness"]] <- rowSums(pptq_data_edit[open_vars])
+  
+  # Conscientiousness
+  consci_vars <- c("pptq4_rev", "pptq9", "pptq14_rev")
+  pptq_score_dat[["pptq_conscientiousness"]] <- rowSums(pptq_data_edit[consci_vars])
+  
+  # Agreeableness
+  agree_vars <- c("pptq5", "pptq10_rev", "pptq15")
+  pptq_score_dat[["pptq_agreeableness"]] <- rowSums(pptq_data_edit[agree_vars])
+  
+  #### 3. Clean Export/Scored Data #####
+  
+  ## merge raw responses with scored data
+  if (isTRUE(ID_arg)){
+    pptq_phenotype <- merge(pptq_data, pptq_score_dat, by = id)
+    
+    return(list(score_dat = as.data.frame(pptq_score_dat),
+                bids_phenotype = as.data.frame(pptq_phenotype)))
+  } else {
+    return(list(score_dat = as.data.frame(pptq_score_dat)))
+  }
+}
+
