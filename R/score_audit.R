ab04b619820a79f191aa6ee2ab0b25a3ddaacf2f
bfuchs18 <bari.fuchs@gmail.com>
2024-05-14 18:21:02 -0400

    update roxygen; add base_zero and extra_scale_cols; output warning if range of data does not meet expectations

--- R/score_audit.R
+++ R/score_audit.R
@@ -2,14 +2,23 @@
 #'
 #' This function scores the Alcohol Use Disorders Identification Test
 #'
-#' To use this function, the data must be prepared according to the following criteria:
-#' 1) The data must include all individual questionnaire items
-#' 2) The  columns/variables must match the following naming convention: 'audit#' or 'audit_#' where # is the question number (1-10)
-#' 3) All questions must have the numeric value for the choice:
+#'#' For data to be scored correctly, the data must be prepared according to the following criteria: \cr
+#' \itemize{
+#'  \item{The data must include all individual questionnaire items}
+#'  \item{The columns/variables must match the following naming convention: 'audit#' or 'audit_#' where # is the question number (1-10)}
+#'  \item{Questionnaire responses for items 1-8 must be a numeric value ranging from 0-4 (base_zero = TRUE) or 1-5 (base_zero = FALSE) where 0/1 is the lowest frequency and 4/5 is the highest frequency}
+#'  \item{Questionnaire responses for items 9-10 must be a numeric value of 0,2,4 (base_zero = TRUE) or 1,3,5 (base_zero = FALSE) where: }
+#'  \itemize{
+#'     \item{For base_zero = TRUE: 0 = No; 2 = Yes, but not in the last year; 4 = Yes, during the last year}
+#'     \item{For base_zero = FALSE: 1 = No; 3 = Yes, but not in the last year; 5 = Yes, during the last year}
+#'   }
+#'  \item{Missing values must be coded as NA}
+#' }
+#' \cr
+#' 
 #' 3a) question 1: 0 - Never, 1 - Monthly or Less, 2 - 2-4 times a month, 3 - 2-3 times a week, 4 - 4 or more times a week
 #' 3b) question 2: 0 - 1 or 2, 1 - 3 or 4, 2 - 5 or 6, 3 - 7 to 9, 4 - 10
 #' 3c) question 3-8: 0 - Never, 1 - Less than Monthly, 2 - Monthly, 3 - Weekly, 4 - Daily or Almost Daily
-#' 3d) questions 9-10: 0 - No, 2 - Yes, but not in the last year, 4 - Yes, during the last year
 #'
 #' Note, as long as variable names match those listed, the dataset can include other variables
 #'
@@ -17,6 +26,7 @@
 #' Saunders JB, Aasland OG, Babor TF, De La Fuente JR, Grant M. Development of the Alcohol Use Disorders Identification Test (AUDIT): WHO Collaborative Project on Early Detection of Persons with Harmful Alcohol Consumption-II. Addiction. 1993;88(6):791-804. doi:10.1111/j.1360-0443.1993.tb02093.x (\href{https://pubmed.ncbi.nlm.nih.gov/8329970/}{PubMed})
 #'
 #' @param audit_data a data.frame all items for the Alcohol Use Disorders Identification Test following the naming conventions described above
+#' @param extra_scale_cols a vector of character strings that begin with 'audit' but are not scale items. Any columns in audit_data that begin with 'audit' but are not scale items must be included here. Default is empty vector.
 #' @inheritParams score_bes
 #'
 #' @return A dataset with a score for the Alcohol Use Disorders Identification Test
@@ -30,7 +40,7 @@
 #'
 #' @export
 
-score_audit <- function(audit_data, id) {
+score_audit <- function(audit_data, id, base_zero = TRUE, extra_scale_cols = c()) {
   
   #### 1. Set up/initial checks #####
   
@@ -52,6 +62,11 @@ score_audit <- function(audit_data, id) {
     }
   }
   
+  # check base_zero is logical
+  if (!is.logical(base_zero)) {
+    stop("base_zero arg must be logical (TRUE/FALSE)")
+  }
+  
   #### 2. Set Up Data #####
   
   # set up database for results create empty matrix
@@ -62,15 +77,53 @@ score_audit <- function(audit_data, id) {
     names(audit_score_dat)[1] <- id
   }
   
-  # remove underscore if in column names
-  names(audit_data) <- gsub('audit_', 'audit', names(audit_data))
+  # assign audit scale items to audit_items, excluding columns in extra_scale_cols
+  audit_items <- grep("^audit", names(audit_data), value = TRUE) %>% setdiff(extra_scale_cols)
   
-  # get primary questions to score
-  q_numbers <- seq(1, 10)
-  audit_primary_qs <- paste0("audit", q_numbers)
+  # remove underscore in column names for audit_items
+  names(audit_data)[names(audit_data) %in% audit_items] <- gsub('audit_', 'audit', names(audit_data)[names(audit_data) %in% audit_items])
+  
+  # remove underscore in audit_items
+  audit_items <- gsub("audit_", "audit", audit_items)
+  
+  # check ranges for items 1-8
+  min = min(audit_data[c("audit1", "audit2", "audit3", "audit4", "audit5", "audit6", "audit7", "audit8")], na.rm = TRUE)
+  max = max(audit_data[c("audit1", "audit2", "audit3", "audit4", "audit5", "audit6", "audit7", "audit8")], na.rm = TRUE)
+  
+  if (isTRUE(base_zero)){
+    if (min < 0 | max > 4) {
+      warning("range in AUDIT data items 1-8 is outside expected range given base_zero = TRUE (expected range: 0-4). Scoring may be incorrect")
+    } 
+  } else {
+    if (min < 1 | max > 5) {
+      warning("range in AUDIT data items 1-8 is outside expected range given base_zero = FALSE (expected range: 1-5). Scoring may be incorrect")
+    } 
+  }
+  
+  # check values for items 9 and 10
+  if (isTRUE(base_zero)){
+    allowed_values = c(0,2,4, NA)
+  } else {
+    allowed_values = c(1,3,5, NA)
+  }
+  
+  if (any(!audit_data$audit9 %in% allowed_values) | any(!audit_data$audit10 %in% allowed_values)) {
+    if (isTRUE(base_zero)){
+      warning("AUDIT data items 9-10 contain unexpected values given base_zero = TRUE (expected values: 0,2,4). Scoring may be incorrect")
+    } else {
+      warning("AUDIT data items 9-10 contain unexpected values given base_zero = FALSE (expected values: 1,3,5). Scoring may be incorrect")
+    }
+  }
+  
+  # rescale to base 0
+  audit_data_edit <- audit_data
+  
+  if (isFALSE(base_zero)){
+    audit_data_edit[audit_items] <- sapply(audit_items, function(x) audit_data_edit[[x]] - 1, simplify = TRUE)
+  }
   
   ## Total Score
-  audit_score_dat[["audit_total"]] <- rowSums(audit_data[audit_primary_qs])
+  audit_score_dat[["audit_total"]] <- rowSums(audit_data_edit[c("audit1", "audit2", "audit3", "audit4", "audit5", "audit6", "audit7", "audit8", "audit9", "audit10")])
   
   # label consumption category
   audit_score_dat[["audit_cat"]] <- ifelse(is.na(audit_score_dat[["audit_total"]]), NA, ifelse(audit_score_dat[["audit_total"]] >=8, "Likely Harmful Consumption", "Not Harmful Consumption"))
