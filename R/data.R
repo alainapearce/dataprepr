388d69c12afb10dd1d0a35b70870ab08a23f4445
bfuchs18 <bari.fuchs@gmail.com>
2024-05-14 11:23:15 -0400

    init documentation for example CEBQ datasets

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' CEBQ Data - Base 0
+#'
+#' An example dataset with base-0 CEBQ data for 2 subjects. 
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name cebq_base0
+#' @usage data(cebq_base0)
+#' @format A data frame with 2 rows and 36 variables. Column 1 is the subject ID ("ID"); columns 2-36 are CEBQ variables ("cebq1" ... "cebq35").
+"cebq_base0"
+
+#' CEBQ Data - Base 1
+#'
+#' An example dataset with base-1 CEBQ data for 2 subjects. 
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name cebq_base1
+#' @usage data(cebq_base1)
+#' @format A data frame with 2 rows and 36 variables. Column 1 is the subject ID ("ID"); columns 2-36 are CEBQ variables ("cebq1" ... "cebq35").
+"cebq_base1"
