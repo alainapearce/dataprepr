36046fcdb06c429ed828561fa2a8399f5febb71a
bfuchs18 <bari.fuchs@gmail.com>
2024-05-15 16:41:57 -0400

    make test df names more generic (test_data) so components can be more easily reused in other test scripts

--- tests/testthat/test-score_cebq.R
+++ tests/testthat/test-score_cebq.R
@@ -6,10 +6,10 @@ cebq_qs <- paste0("cebq", q_numbers) # make character vector of cebq item names
 column_names <- c("ID", cebq_qs) # make character vector with ID variable and cebq item names 
 
 # Initialize dataframe
-test_cebq_base0 <- data.frame(matrix(nrow = 0, ncol = length(column_names)))
+test_data_base0 <- data.frame(matrix(nrow = 0, ncol = length(column_names)))
 
 # add columns names
-colnames(test_cebq_base0) <- column_names
+colnames(test_data_base0) <- column_names
 
 # Make vectors that define items in each Wardle 2001 subscale
 dd_items <- c("cebq6", "cebq29", "cebq31")
@@ -34,26 +34,26 @@ pe_items <- c("cebq7", "cebq10", "cebq16", "cebq24", "cebq32", "cebq33") # Picky
 ee_items <- c("cebq2", "cebq9", "cebq13", "cebq15", "cebq23", "cebq25") # Mean of items 2,9,13,15,23,25.
 
 # Add specific values to row 1 by subscale to facilitate testing 
-test_cebq_base0[1,c("ID")] <- "sub-001" 
-test_cebq_base0[1,dd_items] <- 0 
-test_cebq_base0[1,ef_items] <- 1
-test_cebq_base0[1,eoe_items] <- 2
-test_cebq_base0[1,eue_items] <- 3
-test_cebq_base0[1, ff_items_forwardscore] <- 0
-test_cebq_base0[1, ff_items_reversescore] <- 4 # reverse scores from score_cebq() should be 0
-test_cebq_base0[1, fr_items] <- 3
-test_cebq_base0[1, se_items_forwardscore] <- 0
-test_cebq_base0[1, se_items_reversescore] <- 4 # reverse scores from score_cebq() should be 0
-test_cebq_base0[1, sr_items_forwardscore] <- 0
-test_cebq_base0[1, sr_items_reversescore] <- 4 # reverse scores from score_cebq() should be 0
+test_data_base0[1,c("ID")] <- "sub-001" 
+test_data_base0[1,dd_items] <- 0 
+test_data_base0[1,ef_items] <- 1
+test_data_base0[1,eoe_items] <- 2
+test_data_base0[1,eue_items] <- 3
+test_data_base0[1, ff_items_forwardscore] <- 0
+test_data_base0[1, ff_items_reversescore] <- 4 # reverse scores from score_cebq() should be 0
+test_data_base0[1, fr_items] <- 3
+test_data_base0[1, se_items_forwardscore] <- 0
+test_data_base0[1, se_items_reversescore] <- 4 # reverse scores from score_cebq() should be 0
+test_data_base0[1, sr_items_forwardscore] <- 0
+test_data_base0[1, sr_items_reversescore] <- 4 # reverse scores from score_cebq() should be 0
 
 # generate data with nas
-test_cebq_base0_nas <- test_cebq_base0
-test_cebq_base0_nas[c("cebq1", "cebq2")] <- NA
+test_data_base0_nas <- test_data_base0
+test_data_base0_nas[c("cebq1", "cebq2")] <- NA
 
 # generate data base 1
-test_cebq_base1 <- test_cebq_base0
-test_cebq_base1[2:36] <- test_cebq_base0[2:36] + 1
+test_data_base1 <- test_data_base0
+test_data_base1[2:36] <- test_data_base0[2:36] + 1
 
 #### Make dataframe of expected scores ####
 expected_scores <-
@@ -67,12 +67,12 @@ expected_scores <-
     cebq_se = 1,
     cebq_eue = 4,
     cebq_ff = 1,
-    cebq_approach = as.numeric(round(rowMeans(test_cebq_base0[1,approach_items]),3) + 1),
+    cebq_approach = as.numeric(round(rowMeans(test_data_base0[1,approach_items]),3) + 1),
     cebq_avoid = round(mean(c(rep(3, 4), rep(0, 6), rep(0, 4),rep(0, 5))), 3) + 1,
     #      Mean of items 1,5,12,14,19,20,22,28,34, 3 (reverse), 4 (reverse) ,8 (reverse)
     cebq_rbe = round(mean(c(1, 1, 3, 3, 3, 1, 1, 3, 3, 0, 0, 4)), 3) + 1,
-    cebq_pe = round(rowMeans(test_cebq_base0[1,pe_items]),3) + 1,
-    cebq_ee = round(rowMeans(test_cebq_base0[1,ee_items]),3) + 1
+    cebq_pe = round(rowMeans(test_data_base0[1,pe_items]),3) + 1,
+    cebq_ee = round(rowMeans(test_data_base0[1,ee_items]),3) + 1
   )
 
 expected_scores_nas <-
@@ -86,10 +86,10 @@ expected_scores_nas <-
     cebq_se = 1,
     cebq_eue = 4,
     cebq_ff = 1,
-    cebq_approach = as.numeric(round(rowMeans(test_cebq_base0_nas[1,approach_items]),3) + 1),
+    cebq_approach = as.numeric(round(rowMeans(test_data_base0_nas[1,approach_items]),3) + 1),
     cebq_avoid = round(mean(c(rep(3, 4), rep(0, 6), rep(0, 4),rep(0, 5))), 3) + 1,
     cebq_rbe = as.double(NA),
-    cebq_pe = round(rowMeans(test_cebq_base0_nas[1,pe_items]),3) + 1,
+    cebq_pe = round(rowMeans(test_data_base0_nas[1,pe_items]),3) + 1,
     cebq_ee = as.double(NA)
   )
 
@@ -99,9 +99,9 @@ rownames(expected_scores_nas) <- as.integer(rownames(expected_scores_nas))
 
 #### Run function ####
 
-scored_base0 <- score_cebq(test_cebq_base0, base_zero = TRUE, id = "ID")
-scored_base1 <- score_cebq(test_cebq_base1, base_zero = FALSE, id = "ID")
-scored_base0_nas <- score_cebq(test_cebq_base0_nas, base_zero = TRUE, id = "ID")
+scored_base0 <- score_cebq(test_data_base0, base_zero = TRUE, id = "ID")
+scored_base1 <- score_cebq(test_data_base1, base_zero = FALSE, id = "ID")
+scored_base0_nas <- score_cebq(test_data_base0_nas, base_zero = TRUE, id = "ID")
 
 #### Run tests ####
 test_that("scores accurately computed", {
@@ -120,9 +120,9 @@ test_that("scores accurately computed", {
 
 test_that("bids_phenotype includes input data plus scores", {
 
-  merged <- merge(test_cebq_base0, expected_scores)
+  merged <- merge(test_data_base0, expected_scores)
   
-  # note: this only works because test_cebq_base0 did not have underscores in col names. otherwise, colnames would be modified
+  # note: this only works because test_data_base0 did not have underscores in col names. otherwise, colnames would be modified
   expect_equal(scored_base0$bids_phenotype, merged)
   
   # test removal of underscores in a different dataframe?
@@ -132,18 +132,18 @@ test_that("bids_phenotype includes input data plus scores", {
 test_that("warning if values are out of expected bounds", {
 
   # expect warning if range is 0-4 and base_zero = FALSE
-  expect_warning(score_cebq(test_cebq_base0, base_zero = FALSE))
+  expect_warning(score_cebq(test_data_base0, base_zero = FALSE))
 
   # expect warning if range is 1-5 and base_zero = TRUE
-  expect_warning(score_cebq(test_cebq_base1, base_zero = TRUE))
+  expect_warning(score_cebq(test_data_base1, base_zero = TRUE))
 
 })
 
 test_that("errors if arguments don't meet expectations ", {
 
   # expect error if base_zero isn't logical
-  expect_error(score_cebq(test_cebq_base0, base_zero = "FALSE"))
+  expect_error(score_cebq(test_data_base0, base_zero = "FALSE"))
   
   # expect error if ID isn't in database
-  expect_error(score_cebq(test_cebq_base0, id = "ilovecats"))
+  expect_error(score_cebq(test_data_base0, id = "ilovecats"))
 })
