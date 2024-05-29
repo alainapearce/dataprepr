e8f3747241907cf9d262a91f4ed4e950f642bb4b
bfuchs18 <bari.fuchs@gmail.com>
2024-05-29 16:05:20 -0400

    add pcq_na_value for "not applicable" responses in PCW scale; convert pcq_na_value to NA before scoring PCW scale ignoring NAs; update roxygen

--- R/score_cfq.R
+++ R/score_cfq.R
@@ -2,26 +2,47 @@
 #'
 #' This function scores the Child Feeding Questionnaire and provides subscale scores for the following behaviors: Perceived Responsibility, Perceived Child Weight, Perceived Parent Weight, Child Weight Concerns, Restriction, Pressure to Eat, and Monitoring
 #' 
+#' For all subscales except Perceived Child Weight, scores will only be computed if there are no missing values in subscale items. For Perceived Child Weight, subscale scores will be calculated ignoring missing items and pcw_na_value responses (see Arguments) 
+#' 
 #' For data to be scored correctly, the data must be prepared according to the following criteria: \cr
 #' \itemize{
-#'  \item{The data must include all individual questionnaire items}
-#'  \item{The  columns/variables must match the following naming convention: 'cfq#' or 'cfq_#' where # is the question number (1-31). Question 13 be skipped for subscale - Perceived Child Weight due to age range.}
+#'  \item{The data must include all individual questionnaire items except for question 13:}
+#'  \itemize{
+#'    \item{Question 13 can be excluded from data due to age range. If excluded, Perceived Child Weight will be calculated using items 8-12}
+#'   }
+#'  \item{The columns/variables must match the following naming convention: 'cfq#' or 'cfq_#' where # is the question number (1-31).}
 #'  \item{All questionnaire responses must be a numeric value ranging from 0-4 (base_zero = TRUE) or 1-5 (base_zero = FALSE) where: }
 #'  \itemize{
-#'     \item{For base_zero = TRUE: 0 = X; 1 = X; 2 = X; 3 = X; 4 = X}
-#'     \item{For base_zero = FALSE: 1 = X; 2 = X; 3 = X; 4 = X; 5 = X}
+#'    \item{For base_zero = TRUE:}
+#'    \itemize{
+#'      \item{Items 1-3: 0 = Never; 1 = Seldom; 2 = Half of time; 3 = Most of time; 4 = Always}
+#'      \item{Items 4-13: 0 = Markedly Underweight; 1 = Underweight; 2 = Average; 3 = Overweight; 4 = Markedly Overweight}
+#'      \item{Items 14-16: 0 = Unconcerned; 1 = Slightly unconcerned; 2 = Neutral; 3 = Slightly concerned; 4 = Very Concerned}
+#'      \item{Items 17-28: 0 = Disagree; 1 = Slightly disagree; 2 = Neutral; 3 = Slightly agree; 4 = Agree}
+#'      \item{Items 29-31: 0 = Never; 1 = Rarely; 2 = Sometimes; 3 = Mostly; 4 = Always}
+#'    }
+#'    \item{For base_zero = TRUE:}
+#'    \itemize{
+#'      \item{Items 1-3: 1 = Never; 2 = Seldom; 3 = Half of time; 4 = Most of time; 5 = Always}
+#'      \item{Items 4-13: 1 = Markedly Underweight; 2 = Underweight; 3 = Average; 4 = Overweight; 5 = Markedly Overweight}
+#'      \item{Items 14-16: 1 = Unconcerned; 2 = Slightly unconcerned; 3 = Neutral; 4 = Slightly concerned; 5 = Very Concerned}
+#'      \item{Items 17-28: 1 = Disagree; 2 = Slightly disagree; 3 = Neutral; 4 = Slightly agree; 5 = Agree}
+#'      \item{Items 29-31: 1 = Never; 2 = Rarely; 3 = Sometimes; 4 = Mostly; 5 = Always}
+#'    }
 #'   }
 #'  \item{Missing values must be coded as NA}
+#'  \item{Items 8-13 (Perceived Child Weight subscale) can include responses reflecting "not applicable", as these items ask about certain age ranges. The value encoding "not applicable" must be specified with the pcw_na_value parameter}
 #' }
 #' \cr
 #' Note, as long as variable names match those listed, the dataset can include other variables
 #'
 #' @references
-#' Birch, L. L., Fisher, J. O., Grimm-Thomas, K., Markey, C. N., Sawyer, R., & Johnson, S. L. (2001). Confirmatory factor analysis of the Child Feeng Questionnaire: A measure of parental attitudes, beliefs and practices about child feeng and obesity proneness. Appetite, 36(3), 201–210. https://doi.org/10.1006/appe.2001.0398 (\href{https://pubmed.ncbi.nlm.nih.gov/11358344/}{PubMed})
+#' Birch, L. L., Fisher, J. O., Grimm-Thomas, K., Markey, C. N., Sawyer, R., & Johnson, S. L. (2001). Confirmatory factor analysis of the Child Feeng Questionnaire: A measure of parental attitudes, beliefs and practices about child feeding and obesity proneness. Appetite, 36(3), 201–210. https://doi.org/10.1006/appe.2001.0398 (\href{https://pubmed.ncbi.nlm.nih.gov/11358344/}{PubMed})
 #'
 #' @param cfq_data a data.frame all items for the Child Feeding Questionnaire following the naming conventions described above
+#' @param pcw_na_value (integer) integer used for items 8-13 (Perceived Child Weight items) to indicate "not applicable" response.
 #' @inheritParams score_bes
-#' @param restriction_split a boolean incating if the Restriction subscale should be split to remove food as reward items. Default = FALSE. The standard Restriction subscale will always be available. If restriction_split = TRUE, then two adtional scales will be computed: 1) cfq_rest_noreward: questions 17-20, 23-24 and 1) cfq_foodreward: questions 21-22
+#' @param restriction_split logical indicating if the Restriction subscale should be split to remove food as reward items. Default = FALSE. The standard Restriction subscale will always be available. If restriction_split = TRUE, then two additional scales will be computed: 1) cfq_rest_noreward: questions 17-20, 23-24 and 1) cfq_foodreward: questions 21-22
 #' @param extra_scale_cols a vector of character strings that begin with 'cfq' but are not scale items. Any columns in cfq_data that begin with 'cfq' but are not scale items must be included here. Default is empty vector.
 #' @inheritParams score_bes
 #'
@@ -29,11 +50,11 @@
 #'
 #' @examples
 #'
-#' # scoring for the cfq with IDs
-#' cfq_score_data <- score_cfq(cfq_data, base_zero = TRUE, id = 'ID')
+#' # scoring for the cfq with IDs, with scale from 0-4, and items 8-13 have the value 99 indicating "not applicable"
+#' cfq_score_data <- score_cfq(cfq_data, base_zero = TRUE, id = 'ID', pcw_na_value = 99)
 #'
-#' # scoring for the cfq with extra Restriction subscales
-#' cfq_score_data <- score_cfq(cfq_data, restriction_split = TRUE, id = 'ID')
+#' # scoring for the cfq with extra Restriction subscales, with scale from 1-5
+#' cfq_score_data <- score_cfq(cfq_data, base_zero = FALSE, restriction_split = TRUE, id = 'ID')
 #'
 #' \dontrun{
 #' }
@@ -41,7 +62,7 @@
 #'
 #' @export
 
-score_cfq <- function(cfq_data, base_zero = TRUE, restriction_split = FALSE, id, extra_scale_cols = c()) {
+score_cfq <- function(cfq_data, base_zero = TRUE, restriction_split = FALSE, id, extra_scale_cols = c(), pcw_na_value) {
   
   #### 1. Set up/initial checks #####
   
@@ -112,23 +133,39 @@ score_cfq <- function(cfq_data, base_zero = TRUE, restriction_split = FALSE, id,
   # remove underscore in cfq_items
   cfq_items <- gsub("cfq_", "cfq", cfq_items)
   
-  # check range of data and print warnings
-  min <- min(cfq_data[c(cfq_items)], na.rm = TRUE)
-  max <- max(cfq_data[c(cfq_items)], na.rm = TRUE)
+  # make copy of data
+  cfq_data_edit <- cfq_data
+
+  # if pcw_na_value arg, replace not applicable values with NA
+  if (isTRUE(methods::hasArg(pcw_na_value))) {
+    
+    # define pcw_vars 
+    if ('cfq13' %in% names(cfq_data_edit)) {
+      pcw_vars <- c('cfq8', 'cfq9', 'cfq10', 'cfq11', 'cfq12', 'cfq13')
+    } else {
+      pcw_vars <- c('cfq8', 'cfq9', 'cfq10', 'cfq11', 'cfq12')
+    }
+    
+    # replace pcw_na_value with NA in pcw_vars
+    cfq_data_edit[pcw_vars] <- lapply(cfq_data_edit[pcw_vars], function(x) ifelse(x == pcw_na_value, NA, x))
+    
+  }
+  
+  # check range of data and print warnings (check in cfq_data_edit where not_applicable_value has been replaced with NA)
+  min <- min(cfq_data_edit[c(cfq_items)], na.rm = TRUE)
+  max <- max(cfq_data_edit[c(cfq_items)], na.rm = TRUE)
   
   if (isTRUE(base_zero)){
     if (min < 0 | max > 4) {
-      warning("range in CFQ data is outside expected range given base_zero = TRUE (expected range: 0-4). Scoring may be incorrect")
+      warning("range in CFQ data (excluding pcw_na_value, if specified) is outside expected range given base_zero = TRUE (expected range: 0-4). Scoring may be incorrect")
     } 
   } else {
     if (min < 1 | max > 5) {
-      warning("range in CFQ data is outside expected range given base_zero = FALSE (expected range: 1-5). Scoring may be incorrect")
+      warning("range in CFQ data (excluding pcw_na_value, if specified) is outside expected range given base_zero = FALSE (expected range: 1-5). Scoring may be incorrect")
     } 
   }
   
   # re-scale data to base 1
-  cfq_data_edit <- cfq_data
-  
   if (isTRUE(base_zero)){
     cfq_data_edit[cfq_items] <- sapply(cfq_items, function(x) cfq_data_edit[[x]] + 1, simplify = TRUE)
   }
@@ -140,13 +177,20 @@ score_cfq <- function(cfq_data, base_zero = TRUE, restriction_split = FALSE, id,
   cfq_score_dat[['cfq_resp']] <- rowMeans(cfq_data_edit[resp_vars])
   
   # Perceived Child Weight
-  if ('cfq13' %in% names(cfq_data)) {
+  if ('cfq13' %in% names(cfq_data_edit)) {
     pcw_vars <- c('cfq8', 'cfq9', 'cfq10', 'cfq11', 'cfq12', 'cfq13')
   } else {
     pcw_vars <- c('cfq8', 'cfq9', 'cfq10', 'cfq11', 'cfq12')
   }
-  
-  cfq_score_dat[['cfq_pcw']] <- rowMeans(cfq_data_edit[pcw_vars])
+
+  # calculate the mean across pcw_vars (ignoring NAs), except if all missing values
+  cfq_score_dat[['cfq_pcw']] <- apply(cfq_data_edit[pcw_vars], 1, function(row) {
+    if (all(is.na(row))) {
+      NA
+    } else {
+      mean(row, na.rm = TRUE)
+    }
+  })
   
   # Perceived Parent Weight
   ppw_vars <- c('cfq4', 'cfq5', 'cfq6', 'cfq7')
